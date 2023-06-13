34022edabbcb5ea9e2088d38bfdabd9a8b473b1f
guilherme <guguiejo@gmail.com>
2023-06-13 11:52:20 -0300

    ajeitando pulos

--- js/script.js
+++ js/script.js
@@ -1,8 +1,28 @@
 const mario = document.querySelector('.mario');
-
+const pipe = document.querySelector('.pipe');
 
 const pulo = () => {
     mario.classList.add('pulo');
+    setTimeout(() => {
+        mario.classList.remove('pulo');
+    }, 500);
+
 }
+
+
+const loop = setInterval(()  => {
+    const pipePosition = pipe.offsetLeft;
+    const marioPositon = window.getComputedStyle(mario).bottom;
+
+
+    
+    if (pipePosition <= 120) {
+        
+        pipe.style.animation = 'none';
+        pipe.style.left = ` ${pipePosition}px`;
+
+    }
+
+}, 10) 
 
 document.addEventListener('keydown', pulo);
