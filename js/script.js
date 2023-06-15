25042d7ba2b4cd54da8061bdb9a584fa9355bc7a
guilherme <guguiejo@gmail.com>
2023-06-15 15:44:35 -0300

    quase finalizado

--- js/script.js
+++ js/script.js
@@ -17,10 +17,19 @@ const loop = setInterval(()  => {
 
 
     
-    if (pipePosition <= 120 && pipePosition > 0 && marioPositon < 80) {
+    if (pipePosition <= 120 && pipePosition > 0 && marioPositon < 110) {
         
         pipe.style.animation = 'none';
-        pipe.style.left = ` ${pipePosition}px`;
+        pipe.style.left = `${pipePosition}px`;
+
+        mario.style.animation = 'none';
+        mario.style.bottom = `${marioPositon}px`;
+
+        mario.src = 'imagens/game-over.png';
+        mario.style.width ='75px'
+        mario.style.marginLeft = '50px'
+
+        clearInterval('loop');
 
     }
 
